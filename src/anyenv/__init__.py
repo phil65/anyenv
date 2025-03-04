0d4588967180dea03beef6e332d35ca6c91f104a
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-03-04 13:45:51 +0100

    chore: functional interface

--- src/anyenv/__init__.py
+++ src/anyenv/__init__.py
@@ -1,5 +1,47 @@
+"""AnyEnv: A library for environment management and utility functions."""
+
 __version__ = "0.1.0"
 
+from anyenv.async_run import run_sync, run_sync_in_thread
+from anyenv.download.functional import (
+    download,
+    download_sync,
+    get,
+    get_backend,
+    get_bytes,
+    get_bytes_sync,
+    get_json,
+    get_json_sync,
+    get_sync,
+    get_text,
+    get_text_sync,
+    post,
+    post_sync,
+    request,
+    request_sync,
+)
 from anyenv.threadgroup.threadgroup import ThreadGroup
 
-__all__ = ["ThreadGroup"]
+__all__ = [
+    # ThreadGroup
+    "ThreadGroup",
+    # Download functionality
+    "download",
+    "download_sync",
+    "get",
+    "get_backend",
+    "get_bytes",
+    "get_bytes_sync",
+    "get_json",
+    "get_json_sync",
+    "get_sync",
+    "get_text",
+    "get_text_sync",
+    "post",
+    "post_sync",
+    "request",
+    "request_sync",
+    # Async utilities
+    "run_sync",
+    "run_sync_in_thread",
+]
