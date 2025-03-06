dfa35c5fe3555a7c03a6e41a0811f50f86a76466
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-03-06 20:01:32 +0100

    chore: exceptions

--- src/anyenv/download/exceptions.py
+++ src/anyenv/download/exceptions.py
@@ -1,4 +1,4 @@
-"""Exceptions anyenv HTTP functionality."""
+"""Exceptions for anyenv HTTP functionality."""
 
 from __future__ import annotations
 
@@ -15,3 +15,37 @@ class HttpError(Exception):
     def __init__(self, message: str, response: HttpResponse | None = None):
         super().__init__(message)
         self.response = response
+
+
+class RequestError(HttpError):
+    """Exception raised when a request fails due to network or connection issues."""
+
+    def __init__(self, message: str, original_error: Exception | None = None):
+        super().__init__(message)
+        self.__cause__ = original_error
+
+
+class ResponseError(HttpError):
+    """Exception raised when a response contains an error status code."""
+
+    def __init__(self, message: str, response: HttpResponse):
+        super().__init__(message, response)
+        self.status_code = response.status_code
+
+
+def check_response(response: HttpResponse) -> HttpResponse:
+    """Check response status and raise appropriate exceptions.
+
+    Args:
+        response: The HTTP response to check
+
+    Returns:
+        The same response if status code is < 400
+
+    Raises:
+        ResponseError: If the status code is >= 400
+    """
+    if 400 <= response.status_code < 600:  # noqa: PLR2004
+        message = f"HTTP Error {response.status_code}"
+        raise ResponseError(message, response)
+    return response
