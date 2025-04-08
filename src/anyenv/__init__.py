3d1920f54165edbdfd2623fa232254dd18c5491f
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-04-08 02:00:23 +0200

    Bump version 0.4.13 → 0.4.14

--- src/anyenv/__init__.py
+++ src/anyenv/__init__.py
@@ -1,6 +1,6 @@
 """AnyEnv: A library for environment management and utility functions."""
 
-__version__ = "0.4.13"
+__version__ = "0.4.14"
 
 from anyenv.async_run import run_sync, run_sync_in_thread, gather, run_in_thread
 from anyenv.download.functional import (
