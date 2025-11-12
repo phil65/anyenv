fccb011324baed7be5fc65ad6608ddc83da5f5e7
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-11-12 23:18:09 +0100

    chore: some simple process creation wrappers

--- src/anyenv/__init__.py
+++ src/anyenv/__init__.py
@@ -61,6 +61,7 @@ from anyenv.code_execution.remote_callable import create_remote_callable
 from anyenv.code_execution.base import ExecutionEnvironment
 from anyenv.os_commands import get_os_command_provider
 from anyenv.process_manager import ProcessManager, ProcessOutput, RunningProcess
+from anyenv.processes import create_process, create_shell_process
 
 __all__ = [
     "ExecutionEnvironment",
@@ -81,7 +82,9 @@ __all__ = [
     "TomlLoadError",
     "__version__",
     "call_and_gather",
+    "create_process",
     "create_remote_callable",
+    "create_shell_process",
     "download",
     "download_sync",
     "dump_json",
